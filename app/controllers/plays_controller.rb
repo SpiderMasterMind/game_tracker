0bad996a6336caf7f44aa5f0d38a4b214e417e53
Ben Stacey <benstacey@MacBook-Pro.local>
2017-11-06 16:30:12 +0000

    start common button styles

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -14,9 +14,9 @@ class PlaysController < ApplicationController
 			flash[:notice] = "Play Session Saved!"
 			redirect_to root_path
 		else 			
-			flash[:errors] = "Incorrect players or game info"
+			# flash[:errors] = "Incorrect players or game info"
 			puts new_play_path
-			render 'new'
+			render :new
 		end
 
 
--- app/models/play.rb
+++ app/models/play.rb
@@ -2,4 +2,13 @@ class Play < ActiveRecord::Base
 	has_many :plays_users
 	has_many :users, through: :plays_users
 	belongs_to :game
+	# validate :users_cannot_exceed_game_participant_number
+
+
+	# def users_cannot_exceed_game_participant_number
+	# 	if number_of_user_ids > game.participant_number
+	# 		errors.add(:players, "too many players for selected game.")
+	# 	end
+
+	# end
 end
